f642869c1e1588658633b00060611f90ef44ffc9
rudnickio <92824628+rudnickio@users.noreply.github.com>
2022-07-15 16:01:15 -0300

    Add files via upload

---
+++ PT-PROv1-C43-Template-main/js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "minions";
+const accessCode3 = "sombra";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  fill("blue");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: qual o nome dos minions", 700,270 );
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: quando tem luz ela aparece", 200, 180);
+
+  // Adicione código para exibir as outras duas perguntas.
+}
